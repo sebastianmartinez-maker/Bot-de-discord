6d606bb3bc8bc6a0a2847cd74889299188486c72
sebastianmartinez-maker <eduardomartinez@manitasalaobraeducate.edu.gt>
2025-04-16 17:11:04 -0600

    primer envio

---
+++ app.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os, discord
+
+load_dotenv()
+
+
+
+token = os.getenv("dt")
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$cara'):
+        await message.channel.send("Sello")
+    else:
+        await message.channel.send(message.content)
+
+client.run(token)
