ee15a389bfc578703cabcb1e173d675b1a884110
sebastianmartinez-maker <eduardomartinez@manitasalaobraeducate.edu.gt>
2025-04-16 18:39:27 -0600

    actualizacion clase bot

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from dotenv import load_dotenv
-import os, discord
-
+import os, discord, botlogic as logic
+from discord.ext import commands
 load_dotenv()
-
-
 
 token = os.getenv("dt")
 # La variable intents almacena los privilegios del bot
@@ -11,23 +9,43 @@ intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
 intents.message_content = True
 # Crear un bot en la variable cliente y transferirle los privilegios
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='29', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$cara'):
-        await message.channel.send("Sello")
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
 
-client.run(token)
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command(name = "psw")
+async def passw(ctx, lenght = 25):
+    x = logic.gen_pass(lenght)
+    await ctx.send(f"🔒contraseña generada:{x}")
+
+@bot.command(name = "yt")
+async def youtube(ctx, *, url):
+    if not ctx.voice_client:
+        if ctx.author.voice:
+            channel = ctx.author.voice.channel
+            await channel.connect()
+        else:
+            await ctx.sent("❌Debes estar conectado a un canal de voz para estar conectado")
+            return
+        
+    async with ctx.typing():
+        player = await logic.YTDLSource, from_url(url, Loop = bot.loop)
+        ctx.voice_client.play(player, ofter = lamda e: print(f"error{e}")if e else None)
+
+    await ctx.send(f"🎶 Reproduciendo ahora: **{player.title}**")
+
+
+
+
+bot.run(token)
---
+++ botlogic.py
@@ -0,0 +1,49 @@
+import random as r, string
+import asyncio, discord, yt_dlp as youtube_dl
+
+def gen_pass(pass_length):
+    elements = string.ascii_letters + string.digits + string.punctuation + string.ascii_uppercase
+    password = ""
+    for i in range(pass_length):
+        password += r.choice(elements)
+    return password
+
+# Configuración de yt-dlp
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+# Clase para manejar el audio descargado
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            data = data['entries'][0]  # Si es una playlist, toma el primer elemento
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
